97d388e0c25373632ad6c02d7281ca460c60c381
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-10 23:13:37 +0600

    Create Shopping Cart to handle Event with parameters

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -5,18 +5,33 @@ import './Bottles.css'
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch('bottles.json')
     .then(res => res.json())
     .then(data=> setBottles(data))
   }, []);
+
+
+
+  const handleAddToCart = bottle =>{
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  }
+
+
   return (
     <div>
-      <h3>Bottles Here: {bottles.length}</h3>
+      <h2>Bottles Available: {bottles.length}</h2>
+      <h4>Cart: {cart.length}</h4>
       <div className="bottle-container">
       {
-        bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+        bottles.map(bottle => <Bottle 
+            key={bottle.id} 
+            bottle={bottle}
+            handleAddToCart={handleAddToCart}
+            ></Bottle>)
       }
       </div>
     </div>

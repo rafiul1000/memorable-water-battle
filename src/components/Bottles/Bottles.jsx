69d3486ed47484c667cd37535eea1f8d0c084171
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-11 10:38:31 +0600

    use effect dependency and load storead id

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,38 +1,46 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
-import './Bottles.css'
+import "./Bottles.css";
+import { addToLS, getStoredCart } from "../../utilities/localStorage";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    fetch('bottles.json')
-    .then(res => res.json())
-    .then(data=> setBottles(data))
+    fetch("bottles.json")
+      .then((res) => res.json())
+      .then((data) => setBottles(data));
   }, []);
 
+  // load cart from local storage
+  useEffect(() => {
+    console.log("called the useEffect", bottles.length);
+    if (bottles.length > 0) {
+      const storedCart = getStoredCart();
+      console.log(storedCart);
+    }
+  }, [bottles]);
 
-
-  const handleAddToCart = bottle =>{
+  const handleAddToCart = (bottle) => {
     const newCart = [...cart, bottle];
     setCart(newCart);
-  }
-
+    addToLS(bottle.id);
+  };
 
   return (
     <div>
       <h2>Bottles Available: {bottles.length}</h2>
       <h4>Cart: {cart.length}</h4>
       <div className="bottle-container">
-      {
-        bottles.map(bottle => <Bottle 
-            key={bottle.id} 
+        {bottles.map((bottle) => (
+          <Bottle
+            key={bottle.id}
             bottle={bottle}
             handleAddToCart={handleAddToCart}
-            ></Bottle>)
-      }
+          ></Bottle>
+        ))}
       </div>
     </div>
   );

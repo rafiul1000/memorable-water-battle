aea29cc167af88800d387fff63c1be8387c64413
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-11 11:22:06 +0600

    Display Saved cart from Local storage

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css";
 import { addToLS, getStoredCart } from "../../utilities/localStorage";
+import Cart from "../Cart/Cart";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
@@ -19,7 +20,21 @@ const Bottles = () => {
     console.log("called the useEffect", bottles.length);
     if (bottles.length > 0) {
       const storedCart = getStoredCart();
-      console.log(storedCart);
+      console.log(storedCart, bottles);
+
+
+      const savedCart = [];
+
+      for(const id of storedCart){
+        console.log(id);
+        const bottle = bottles.find(bottle => bottle.id === id);
+        if(bottle){
+          savedCart.push(bottle)
+        }
+      }
+      console.log('saved cart: ', savedCart);
+      setCart(savedCart);
+
     }
   }, [bottles]);
 
@@ -32,7 +47,9 @@ const Bottles = () => {
   return (
     <div>
       <h2>Bottles Available: {bottles.length}</h2>
-      <h4>Cart: {cart.length}</h4>
+
+      <Cart cart={cart}></Cart>
+
       <div className="bottle-container">
         {bottles.map((bottle) => (
           <Bottle
